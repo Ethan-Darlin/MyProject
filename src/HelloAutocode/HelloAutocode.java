e00c6f3686a5ea888cb3d755c7aefb7c23ac42e2
Ivan Erokhovets <ivan.erkhovets@mail.ru>
2022-06-29 11:27:18 +0300

    v.1.1

---
+++ src/HelloAutocode/HelloAutocode.java
@@ -0,0 +1,11 @@
+package HelloAutocode;
+
+import java.io.IOException;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello,Autocode!");
+    }
+}
+
+
